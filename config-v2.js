b04b1fe35c2774877d2d47c36414e1ae262ae534
Fxbaby <154943659+Babyfx@users.noreply.github.com>
2024-11-07 08:54:39 +0300

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "https://github.com/Babyfx/PRABATHMD"
+const BOT_NUMBER = "256761396309"
+const SESSION_ID = "PRABATH-MD~czN3CZ5Z#xiTt0hr3_7xteJpRV6z38Fzv-aiKC2_c3ep_p8JJqjY"
 
 
 module.exports = {
